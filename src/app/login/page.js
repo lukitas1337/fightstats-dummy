6a5c9a9c0fd707f72e88326839101c6301d70fc8
Lukitas <lvd.follert@gmail.com>
2025-01-06 11:34:30 +0100

    fa import, sign in button

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
 export default function LoginPage() {
     return (
     <div>
-        <div className="bg-white border p-4 max-w-sm mx-auto">
-            <h1 className='text-4xl font-bold text-center mb-6'>Sign In</h1>
-        <button className='bg-blue-500 text-white px-6 py-4 text-center w-full'>Sign In with Google</button>
+        <div className="p-4 max-w-xs mx-auto">
+            <h1 className='text-4xl font-bold text-center mb-2'>Sign In</h1>
+            <p className='text-center text-gray-600 mb-6'>Sign in to your account using one of the methods below.</p>
+
+        <button className='bg-blue-500 text-white py-4 text-center w-full flex gap-4 items-center justify-center'>
+            <FontAwesomeIcon icon={faGoogle} className='icon-large'/>
+            <span>Sign In with Google</span>
+        </button>
         </div>
     </div>
     )
