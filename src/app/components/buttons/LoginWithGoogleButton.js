ad6bae43455085a2868050950265e11ab82b0b45
Lukitas <lvd.follert@gmail.com>
2025-01-07 11:15:11 +0100

    added route.js with GoogleProvider

--- src/app/components/buttons/LoginWithGoogleButton.js
+++ src/app/components/buttons/LoginWithGoogleButton.js
@@ -1,9 +1,12 @@
+'use client';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
-export default function LoginWithGoogle() {
+export default function LoginWithGoogleButton() {
     return (
         <button 
+        onClick={() => {}}
         className='bg-white shadow py-4 text-center w-full flex gap-4 items-center justify-center'>
             <FontAwesomeIcon icon={faGoogle} className='icon-large'/>
             <span>Sign In with Google</span>
