6b03fe2bc6b8748070ec1da6da02a5acf29411fc
Lukitas <lvd.follert@gmail.com>
2025-01-09 13:43:39 +0100

    see last

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "../mongodb.js";  // Changed this line to use relative import
+import clientPromise from "../mongodb.js";
 
 export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
